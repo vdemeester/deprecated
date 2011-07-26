55059418472d26e409bab38d31afb5405bd3ac25
Vincent Demeester <vincent@demeester.fr>
2011-07-26 10:58:15 +0200

    get -> git

--- xinitrc.d/composite.sh
+++ xinitrc.d/composite.sh
@@ -6,5 +6,5 @@ if [ -x $DCOMPMGR ]; then
     $DCOMPMGR &
 else
     echo "No dcompmgr. You might want to get it :"
-    echo "get clone git://git.openbox.org/dana/dcompmgr"
+    echo "git clone git://git.openbox.org/dana/dcompmgr"
 fi
