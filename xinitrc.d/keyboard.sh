501a619c81706448418e3c4c0f348917a8101ee8
Vincent Demeester <vincent.demeester@nextep.net>
2011-11-06 14:41:47 +0100

    Swap left alt & left win (compose) on the macbook

--- xinitrc.d/keyboard.sh
+++ xinitrc.d/keyboard.sh
@@ -5,6 +5,10 @@ setxkbmap fr bepo-sbr
 setxkbmap -option compose:caps
 # Enable CTRL+ALT+BACKSPACE to terminate X
 setxkbmap -option terminate:escape
+if test "$(hostname)" = "vinnsento"; then
+    # vinnsento is a macbook so we switch the left alt and the win (Compose) keys
+    setxkbmap -option altwin:swap_lalt_lwin
+fi
 # call Xmodmap
 xmodmap ~/.Xmodmap
 
