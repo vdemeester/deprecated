673cfeb0a73e8ad4fe2a5ba22fd19cf6d187d2c8
Vincent Demeester <vincent@demeester.fr>
2011-12-22 10:36:29 +0100

    Fixing copyright header :-/

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: rubygem-mysql
 # Recipe:: default
 #
-# Copyright 2011, Mike Adolphs
+# Copyright 2011, Vincent Demeester
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
